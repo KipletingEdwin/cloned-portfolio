1109478355238a4076fb21c80db2168aaad2e255
Kipleting Edwin <kipletingedwin4@gmail.com>
2023-12-30 10:54:37 +0300

    add hamburger icon

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,15 @@
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "./NavbarStyles.css";
+import { FaBars, FaTimes} from "react-icons/fa";
 
-import React from 'react'
+
 
 function Navbar() {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
   return (
     <div className="header">
         <Link to="/"> 
@@ -26,6 +32,12 @@ function Navbar() {
                 <Link to="/contact">Contact</Link>
             </li>
         </ul>
+        <div className="hamburger" onClick={handleClick}>
+            {click ?  (<FaTimes size={20} style={{color: "#fff"}} />) : (<FaBars size={20} style={{color: "#fff"}} />) }
+            
+           
+
+        </div>
 
     </div>
   )
