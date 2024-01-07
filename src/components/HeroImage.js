365c9b15bc0d61ead40eae672c1641c9ddde0d56
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-01-07 10:32:08 +0300

    add hero image section

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,10 +1,24 @@
 import "./HeroImageStyles.css";
+import introImage from "../assets/intro-bg.jpg"
 
 import React from 'react'
+import { Link } from "react-router-dom";
 
 function HeroImage() {
   return (
-    <div>HeroImage</div>
+    <div className="hero">
+      <div className="mask">
+        <img className="intro-img" src={introImage} alt="introImage"/>
+      </div>
+      <div className="content">
+        <p>I'M A FREELANCER</p>
+        <h1>React Developer</h1>
+        <div>
+          <Link to="/project" className="btn">Projects</Link>
+          <Link to="/contact" className="btn btn-light">Contact</Link>
+        </div>
+      </div>
+      </div>
   )
 }
 
